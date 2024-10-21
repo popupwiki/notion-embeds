8d30fef5f16dc438a30f3afe573d3b04da8abfff
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-10-22 02:42:05 +0900

    🐛 위젯 UI가 안뜨는 버그 픽스
    Href가 forecase7 가 아닌 경우 UI가 전혀 나타나지 않음. WeatherScript 내부적으로 href가 정해져 있는 것 같음.

--- src/app/embeds/weather/hongdae/page.tsx
+++ src/app/embeds/weather/hongdae/page.tsx
@@ -6,14 +6,12 @@ const HongdaeWeatherWidget = () => {
       <WeatherScript />
       <a
         className="weatherwidget-io"
-        href="https://www.kr-weathernews.com/mv4/html/today.html?loc=101115"
+        href="https://forecast7.com/en/37d55126d91/hapjeong-dong/"
         data-label_1="홍대/합정"
         data-label_2="날씨"
         data-icons="Climacons Animated"
         data-theme="pure"
-      >
-        홍대/합정 날씨
-      </a>
+      />
     </>
   );
 };
